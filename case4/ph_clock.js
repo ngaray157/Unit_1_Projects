108999f2db5059e3ccd9bd850f71ed84d0ade2e2
Nicole Garay <ngaray157@west-mec.org>
2020-01-22 13:28:47 -0700

    finished case 4

--- case4/ph_clock.js
+++ case4/ph_clock.js
@@ -13,18 +13,27 @@
 */
 
 //Global Variables
-var minsLeft = 0; //used to track the number of minutes left to submit order
+var minsLeft = 30; //used to track the number of minutes left to submit order
 var secsLeft = 15; //used to track the number of seconds left to submit order
 var timeLeft = minsLeft * 60 + secsLeft; //stores the number of seconds left to submit a ticket order
+var clockID = setInterval("countdown()", 1000); //this makes the number decrease by 1 second 1000ms = 1s
 
-checkTimer();
+
 function countdown(){
-   minsLeft = timeLeft / 60 + Math.floor();
+   minsLeft = Math.floor(timeLeft / 60);
    secsLeft = timeLeft - 60 * minsLeft;
-   var minsString = numStr;
+   var minsString = addLeadingZero(minsLeft);
    var secsString = addLeadingZero(secsLeft);
    document.getElementById("minutes").innerHTML = minsString;
    document.getElementById("seconds").innerHTML = secsString;
+   checkTimer(); //this makes the countdown() function check the timer and if it = 0, the clock stops
+   timeLeft--; //this decreases the time left by one each second
+}
+
+//this function stops the clock once the time to submit the order has run out
+function stopClock(){
+   document.getElementById("TimeHead").insertAdjacentHTML('beforeEnd', "<br />");
+   clockID = clearInterval(clockID);
 }
 
 
